8d33bd2c1a9662e343aa4555ee01bbfc5e07ddbb
Felipe <felipe-mattioli98@hotmail.com>
2021-02-08 22:38:04 -0300

    Finalizando cadastro de usuários e configurando repositorios

---
+++ GerenciadorCondominios.DAL/Repositorios/RepositorioGenerico.cs
@@ -0,0 +1,127 @@
+﻿using GerenciadorCondominios.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorCondominios.DAL.Repositorios
+{
+    public class RepositorioGenerico<TEntity> : IRepositorioGenerico<TEntity> where TEntity : class
+    {
+
+        private readonly Contexto _contexto;
+
+        public RepositorioGenerico(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task Atualizar(TEntity entity)
+        {
+            try
+            {
+                _contexto.Set<TEntity>().Update(entity);
+                await _contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Exluir(TEntity entity)
+        {
+            try
+            {
+                _contexto.Set<TEntity>().Remove(entity);
+                await _contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Exluir(int id)
+        {
+            try
+            {
+                var entity = await PegarPeloId(id);
+                _contexto.Set<TEntity>().Remove(entity);
+                await _contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Exluir(string id)
+        {
+            try
+            {
+                var entity = await PegarPeloId(id);
+                _contexto.Set<TEntity>().Remove(entity);
+                await _contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Inserir(TEntity entity)
+        {
+            try
+            {
+                await _contexto.AddAsync(entity);
+                await _contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<TEntity> PegarPeloId(int id)
+        {
+            try
+            {
+                return await _contexto.Set<TEntity>().FindAsync(id);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<TEntity> PegarPeloId(string id)
+        {
+            try
+            {
+                return await _contexto.Set<TEntity>().FindAsync(id);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<IEnumerable<TEntity>> PegarTodos()
+        {
+            try
+            {
+                return await _contexto.Set<TEntity>().ToListAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
--- GerenciadorCondominios/Startup.cs
+++ GerenciadorCondominios/Startup.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GerenciadorCondominios.BLL.Models;
 using GerenciadorCondominios.DAL;
+using GerenciadorCondominios.DAL.Interfaces;
+using GerenciadorCondominios.DAL.Repositorios;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +29,10 @@ namespace GerenciadorCondominios
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<Contexto>(opcoes => opcoes.UseSqlServer(Configuration.GetConnectionString("ConexaoDB")));
+            services.AddIdentity<Usuario, Funcao>().AddEntityFrameworkStores<Contexto>();
+            services.AddAuthentication();
+            services.AddAuthorization();
+            services.AddTransient<IUsuarioRepositorio, UsuarioRepositorio>();
             services.AddControllersWithViews();
         }
 
@@ -44,9 +51,8 @@ namespace GerenciadorCondominios
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
