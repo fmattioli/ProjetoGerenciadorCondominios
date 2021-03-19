a7f231ebb54fbf78ee7e460c654243755f1aca59
Felipe <felipe-mattioli98@hotmail.com>
2021-03-18 22:30:31 -0300

    Cadastro de eventos

--- GerenciadorCondominios/Controllers/EventosController.cs
+++ GerenciadorCondominios/Controllers/EventosController.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using GerenciadorCondominios.BLL.Models;
-using GerenciadorCondominios.DAL;
+﻿using GerenciadorCondominios.BLL.Models;
 using GerenciadorCondominios.DAL.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace GerenciadorCondominios.Controllers
 {
+    [Authorize]
     public class EventosController : Controller
     {
         private readonly IEventoRepositorio _eventoRepositorio;
@@ -18,19 +14,19 @@ namespace GerenciadorCondominios.Controllers
 
         public EventosController(IEventoRepositorio eventoRepositorio, IUsuarioRepositorio usuarioRepositorio)
         {
-            this._eventoRepositorio = eventoRepositorio;
-            this._usuarioRepositorio = usuarioRepositorio;
+            _eventoRepositorio = eventoRepositorio;
+            _usuarioRepositorio = usuarioRepositorio;
         }
 
         // GET: Eventos
         public async Task<IActionResult> Index()
         {
             Usuario usuario = await _usuarioRepositorio.PegarUsuarioPeloNome(User);
-            if(await _usuarioRepositorio.VerificarSeUsuarioEstaEmFuncao(usuario, "Morador"))
+
+            if (await _usuarioRepositorio.VerificarSeUsuarioEstaEmFuncao(usuario, "Morador"))
             {
                 return View(await _eventoRepositorio.PegarEventosPeloId(usuario.Id));
             }
-
             return View(await _eventoRepositorio.PegarTodos());
         }
 
@@ -38,13 +34,14 @@ namespace GerenciadorCondominios.Controllers
         public async Task<IActionResult> Create()
         {
             Usuario usuario = await _usuarioRepositorio.PegarUsuarioPeloNome(User);
-            Evento evento = new Evento()
+            Evento evento = new Evento
             {
                 UsuarioId = usuario.Id
             };
-           
+
             return View(evento);
         }
+
 
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -53,10 +50,10 @@ namespace GerenciadorCondominios.Controllers
             if (ModelState.IsValid)
             {
                 await _eventoRepositorio.Inserir(evento);
-                TempData["NovoRegistro"] = $"Evento {evento.Nome} inserido com sucesso!!";
+                TempData["NovoRegistro"] = $"Evento {evento.Nome} inserido com sucesso";
                 return RedirectToAction(nameof(Index));
             }
-            
+
             return View(evento);
         }
 
@@ -72,7 +69,6 @@ namespace GerenciadorCondominios.Controllers
             return View(evento);
         }
 
-       
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("EventoId,Nome,Data,UsuarioId")] Evento evento)
@@ -85,22 +81,20 @@ namespace GerenciadorCondominios.Controllers
             if (ModelState.IsValid)
             {
                 await _eventoRepositorio.Atualizar(evento);
-                TempData["Atualizacao"] = $"Evento {evento.Nome} atualizado!!!";
+                TempData["Atualizacao"] = $"Evento {evento.Nome} atualizado";
                 return RedirectToAction(nameof(Index));
             }
+
             return View(evento);
         }
 
-       
-        // POST: Eventos/Delete/5
+
         [HttpPost]
         public async Task<JsonResult> Delete(int id)
         {
-             await _eventoRepositorio.Excluir(id);
-            TempData["Exclusao"] = $"Evento excluido com sucesso!!!";
+            await _eventoRepositorio.Excluir(id);
+            TempData["Exclusao"] = $"Evento excluído";
             return Json("Evento excluído");
         }
-
-        
     }
 }
